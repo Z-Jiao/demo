338274ff77080a57a374c518359f764707e16820
Z-Jiao <838855804@qq.com>
2019-10-13 17:27:01 +0800

    add sql ddl

--- src/main/java/com/community/controller/AuthorizeController.java
+++ src/main/java/com/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.community.controller;
 
 import com.community.dto.AccessTokenDTO;
 import com.community.dto.GitHubUser;
+import com.community.mapper.UserMapper;
+import com.community.model.User;
 import com.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,10 +11,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -20,8 +28,10 @@ public class AuthorizeController {
     private String clientsecret;
     @Value("${github.client.uri}")
     private String uri;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientsecret);
@@ -29,8 +39,24 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(uri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
-        GitHubUser user = gitHubProvider.gitUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GitHubUser gitHubUser = gitHubProvider.gitUser(accessToken);
+
+        if (gitHubUser != null) {
+            request.getSession().setAttribute("user", gitHubUser);
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(gitHubUser.getName());
+            user.setAccount_id(String.valueOf(gitHubUser.getId()));
+            user.setGmt_create(System.currentTimeMillis());
+            user.setGmt_modified(user.getGmt_create());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", gitHubUser);
+            return "redirect:/";
+            //登录成功，写cookie和session
+        } else {
+            //登陆失败
+            return "redirect:/";
+        }
+
     }
 }
--- src/main/java/com/community/controller/IndexController.java
+++ src/main/java/com/community/controller/IndexController.java
@@ -1,11 +1,8 @@
 package com.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
 
 @Controller
 public class IndexController {
